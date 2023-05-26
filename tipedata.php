273b43a6004b268b1b7291ad1beb1b4adb503ee7
AlamNur99 <muh.alamnur.haris99@gmail.com>
2023-05-26 09:11:31 +0800

    tipe data

---
+++ tipedata.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = "ini adalah String";
+
+//tipe data integer
+$b = 50;
+
+//tipe data float
+$c = 5.0;
+
+//tipe data boolean (true or false)
+$d = true;
+
+//array
+$e = ["a", "b", "c",];
+
+//null
+$f = null;
+
+
+//untuk mengetahui tipe data dari sebuah variabel
+echo var_dump($b);
+
+echo var_dump($e);
